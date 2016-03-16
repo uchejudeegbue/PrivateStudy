6ed366ae77e4f5404e498d2f041f995d023a64ec
uchejudeegbue <lordadmiralty@yahoo.com>
2016-03-17 00:18:28 +0100

    Private

--- PhpStudy/index.php
+++ PhpStudy/index.php
@@ -97,5 +97,40 @@ echo "<br>";
 echo $x + $y;
 echo "<br>";
 
+/*Operators are used to perform operations on variables and values.
+
+PHP divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Increment/Decrement operators
+Logical operators
+String operators
+Array operators*/
+
+$x = 20;
+$x += 100;
+
+echo $x;
+echo "<br>";
+
+$x = 10;
+$x /= 5;
+
+echo $x;
+echo "<br>";
+
+$x = 15;
+$x %= 4;
+
+echo $x;
+echo "<br>";
+
+$x = 100;
+$y = "100";
+
+var_dump($x != $y); // returns false because values are equal
+
 
 ?>
