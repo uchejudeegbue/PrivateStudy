05fbc1a312950f075f581b915c8809bfe3ea9221
uchejudeegbue <lordadmiralty@yahoo.com>
2016-03-16 04:54:37 +0100

    Private

--- PhpStudy/index.php
+++ PhpStudy/index.php
@@ -64,5 +64,5 @@ command (notice that the text can contain HTML markup):*/
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello World! <br>";
 echo "I am learning PHP! <br>";
-echo "This ", "string ", "was ", "made ", "with multiple ", "parameters.";
+echo "This " . "string " . "was ", "made ", "with multiple ", "parameters.";
 ?>
