eff7fdcb523c655676a2904b072c3a09265b735e
uchejudeegbue <lordadmiralty@yahoo.com>
2016-03-16 03:38:21 +0100

    Private

--- PhpStudy/index.php
+++ PhpStudy/index.php
@@ -38,4 +38,25 @@ $color = "red";
 echo "My car is " . $color . "<br>";
 echo "My house is " . $COLOR . "<br>";
 echo "My boat is " . $coLOR . "<br>";
+/*A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).
+
+Rules for PHP variables:
+
+A variable starts with the $ sign, followed by the name of the variable
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive ($age and $AGE are two different variables)*/
+$txt = "W3Schools.com";
+echo "I love $txt!";
+echo "<br>";
+//The example below will produce the same result with the above
+$txt = "W3Schools.com";
+echo "I love" . $txt . "!";
+echo "<br";
+//The following example will output the sum of two variables:
+$x = 5;
+$y = 4;
+echo $x + $y;
+echo "<br>";
 ?>
