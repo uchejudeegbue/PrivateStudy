2ec1e23f73b93f645e08d02abbe16f1dd3db6de2
uchejudeegbue <lordadmiralty@yahoo.com>
2016-03-16 03:24:00 +0100

    Private

--- PhpStudy/index.php
+++ PhpStudy/index.php
@@ -14,7 +14,7 @@ It is a good programming practice to put comments in your codes
 to enable you remember what you have done sometime after*/
 echo "Print Again";
 // echo - is used to print out things
-//$ - is used to make input data
+//$ - is used to make input variables
 //use semi colon to end any line in php
 //<br> is used for line break as it is done in html
 // Note that you can use comments to leave out parts of a code line
@@ -28,4 +28,12 @@ are not case sensitive. See examples below*/
 ECHO "Printing <br>";
 Echo "Printing <br>";
 echo "Printing <br>";
+/* All variables are case sensitive
+Below, only the first statement will display the $color variable
+because $color, $COLOR and $coLOR are treated as different variables
+Also note that we use the dot sign , "." , to concantenate*/
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My car is " . $COLOR . "<br>";
+echo "My car is " . $coLOR . "<br>";
 ?>
