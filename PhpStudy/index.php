17a4ebb5801d10c6dea6c26d43c5d9f3c8288183
uchejudeegbue <lordadmiralty@yahoo.com>
2016-03-16 03:07:03 +0100

    Private

--- PhpStudy/index.php
+++ PhpStudy/index.php
@@ -6,4 +6,16 @@
  * Time: 2:49 AM
  */
 echo "Print";
+// This is a single line comment. Single Line Comments do not print
+/* This is a multiple line comment.
+comments that span into multiple lines.
+Multiple line comments do not print.
+It is a good programming practice to put comments in your codes
+to enable you remember what you have done sometime after*/
+echo "Print Again";
+// echo - is used to print out things
+//$ - is used to make inputs
+//use semi colon to end any line in php
+// Note that you can use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
 ?>
