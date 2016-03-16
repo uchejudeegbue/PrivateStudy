cb0a04bcfd7a286816100853f99991377ad6285c
uchejudeegbue <lordadmiralty@yahoo.com>
2016-03-16 03:09:14 +0100

    Private

--- PhpStudy/index.php
+++ PhpStudy/index.php
@@ -14,9 +14,10 @@ It is a good programming practice to put comments in your codes
 to enable you remember what you have done sometime after*/
 echo "Print Again";
 // echo - is used to print out things
-//$ - is used to make inputs
+//$ - is used to make input data
 //use semi colon to end any line in php
 //<br> is used for line break as it is done in html
 // Note that you can use comments to leave out parts of a code line
 $x = 5 /* + 15 */ + 5;
+echo $x;
 ?>
