971345e366f8b7fe442c7257cc05a79f0c1ee92c
uchejudeegbue <lordadmiralty@yahoo.com>
2016-03-17 01:48:33 +0100

    Private

--- PhpStudy/index.php
+++ PhpStudy/index.php
@@ -231,4 +231,17 @@ switch ($favcolor) {
         echo "Your favorite color is neither red, blue, nor green!";
 }
 echo "<br>";
+
+/*In PHP, we have the following looping statements:
+while - loops through a block of code as long as the specified condition is true*/
+
+
+//do...while - loops through a block of code once, and then repeats the loop as long as the specified condition is true
+
+
+//for - loops through a block of code a specified number of times
+
+
+//foreach - loops through a block of code for each element in an array
+
 ?>
