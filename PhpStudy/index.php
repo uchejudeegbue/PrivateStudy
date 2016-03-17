07c0e0e21f420d261edc3385b2b651a1f8dc8cb5
uchejudeegbue <lordadmiralty@yahoo.com>
2016-03-17 01:44:13 +0100

    Private

--- PhpStudy/index.php
+++ PhpStudy/index.php
@@ -212,8 +212,23 @@ if ($t <  "10") {
     echo "Have a good night!";
 }
 echo "<br>";
+
 //switch statement - selects one of many blocks of code to be executed
 
+$favcolor = "red";
 
-
+switch ($favcolor) {
+    case "red":
+        echo "Your favorite color is red!";
+        break;
+    case "blue":
+        echo "Your favorite color is blue!";
+        break;
+    case "green":
+        echo "Your favorite color is green!";
+        break;
+    default:
+        echo "Your favorite color is neither red, blue, nor green!";
+}
+echo "<br>";
 ?>
