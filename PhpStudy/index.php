4b4518a4d3d907b69b16681bbfcc319b83e83c76
uchejudeegbue <lordadmiralty@yahoo.com>
2016-03-20 22:16:11 +0100

    Private

--- PhpStudy/index.php
+++ PhpStudy/index.php
@@ -374,9 +374,25 @@ Wanted ("Joe", $specs, $mugs, $sR, $awardfCap);
 
 echo "<br>";
 
+//function to set the minimum height
 function setHeight($minheight) {
     echo "The height is : $minheight <br>";
 }
 
 setHeight(60);
+
+echo "<br>";
+
+/*An array is a special variable, which can hold more than one value at a time.
+If you have a list of items (a list of car names, for example), storing the
+cars in single variables could look like this:
+$cars1 = "Volvo";
+$cars2 = "BMW";
+$cars3 = "Toyota";
+However, what if you want to loop through the cars and find a specific one? And
+what if you had not 3 cars, but 300? The solution is to create an array! An array
+can hold many values under a single name, and you can access the values by referring to an index number.
+
+
+
 ?>
