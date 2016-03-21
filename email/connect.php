4bc724716bc6566aeb6a75b73848f86d965c60e1
uchejudeegbue <lordadmiralty@yahoo.com>
2016-03-21 05:17:03 +0100

    Private

---
+++ email/connect.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: UCHE EGBUE BENDAC
+ * Date: 3/21/2016
+ * Time: 4:36 AM
+ */
+define ('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define ('DB_USERNAME', 'b7b1c7b711b6c5');
+define ('DB_PASSWORD', '76437e9e');
+define ('DB_DATABASE', 'uche_1418349');
+$db = mysqli_connect (DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+?>
