2ed10ced26c99f95d295d5337cb4f5a79e3cbd4d
uchejudeegbue <lordadmiralty@yahoo.com>
2016-04-01 20:56:35 +0100

    Private

--- PhpLab/index.php
+++ PhpLab/index.php
@@ -70,7 +70,7 @@ The people of Aberdeen are still struggling to understand at what age they are a
 rolls. Make a webpage that stores a variable called $wantedGood. Create a switch statement that takes this variable
 and prints out how old you have to be.*/
 
-$wantedGood = specs;
+$wantedGood = mugs;
 
 switch ($wantedGood){
     case specs:
