75f71d2df5ff412a35b3dba2bb365c203c881a31
uchejudeegbue <lordadmiralty@yahoo.com>
2016-04-03 00:58:10 +0100

    Private

--- users/login.php
+++ users/login.php
@@ -11,8 +11,8 @@ if (empty ($_POST["username"])|| empty ($_POST["password"])){
     echo "Both fields are required.";
 }
 else {
-    $username=$_POST['username'];
-    $password=$_POST['password'];
+    $username= $_POST['username'];
+    $password= $_POST['password'];
 }
 $sql="SELECT * FROM users WHERE username = '$username' and password=$'password'";
 $result=mysqli_query($db,$sql);
