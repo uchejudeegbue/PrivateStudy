8a05f2136723fe534ab70f647635079ca63f159a
1418349 <a@a.com>
2016-04-11 09:55:15 +0100

    cookie

---
+++ cookie/connection.php
@@ -0,0 +1,8 @@
+<?php
+define ('DB_SERVER','ap-cdbr-azure-east-c.cloudapp.net' );
+define ('DB_USERNAME', 'bb3d9d0e1d715c');
+define ('DB_PASSWORD', '5d131267');
+define ('DB_DATABASE','cookie');
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+?>
