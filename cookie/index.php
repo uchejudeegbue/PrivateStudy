9c9424616f927cc8ecd71c0aded0f4be15b9aa08
1418349 <a@a.com>
2016-04-11 10:39:02 +0100

    cookie

--- cookie/index.php
+++ cookie/index.php
@@ -1,7 +1,20 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1418349
- * Date: 11/04/2016
- * Time: 10:09
- */
+<!DOCTYPE html>
+    <html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Login Page</title>
+
+</head>
+
+<body>
+<form action="checklogin.php" method="post">
+    Name:<br>
+    <input type="text" name="name"/>
+    <br>
+    Password: <br>
+    <input type="password" name="password"/>
+    <br><br>
+    <input type="submit" value="submit"/>
+</form>
+</body>
+</html>
