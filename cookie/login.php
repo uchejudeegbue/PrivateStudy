ce5cb7bd070757ceffed6bb2468504f2eb2f5f43
1418349 <a@a.com>
2016-04-11 10:41:34 +0100

    cookie

--- cookie/login.php
+++ cookie/login.php
@@ -5,13 +5,13 @@ $accesslevel = $_COOKIE['access_level_cookie'];
 
 displayAccessLevelInformation($accesslevel);
 
-function displayAccessLevelInformation($accesslevel){
-    if ($accesslevel == "standarduser"){
+function displayAccessLevelInformation($accesslevel)
+{
+    if ($accesslevel == "standarduser") {
         echo "<p style = \"background-color: lightgreen\"> You are currently logged in as a standard user</p>";
+    } elseif ($accesslevel == "root") {
+        echo "<p style = \"background-color: red\"> You are currently logged in as a root user</p>";
+        echo "<p style = \"background-color: red\"> You now have access to additional administrative features</p>";
     }
-    elseif ($accesslevel == "root"){
-    echo "<p style = \"background-color: red\"> You are currently logged in as a root user</p>";
-    echo "<p style = \"background-color: red\"> You now have access to additional administrative features</p>";
 }
-
 ?>
