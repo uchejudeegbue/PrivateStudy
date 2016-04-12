ef181e43c427e62185b21fb2bcf212ffb3fbbd8f
uchejudeegbue <lordadmiralty@yahoo.com>
2016-04-12 07:02:09 +0100

    Private

--- superH/server.php
+++ superH/server.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: UCHE EGBUE BENDAC
- * Date: 4/12/2016
- * Time: 6:31 AM
- */
+
+define ("SERVER", 'ap-cdbr-azure-east-c.cloudapp.net');
+define ("USERNAME", 'b1463873078bfe');
+define ("PASSWORD", 'b3b9d9ca');
+define ("DATABASE", 'superH');
+
+$db = mysqli_connect(SERVER, USERNEM, PASSWORD, DB);
+
+// test if connection was established and print any errors
+
+if (!db){
+    die ("connection failed:" . mysqli_connect_error ());
+}else {
+
+}
+?>
