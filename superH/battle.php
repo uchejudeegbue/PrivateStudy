a1e62722f958d7528487bfac946f8c679a69a1e3
uchejudeegbue <lordadmiralty@yahoo.com>
2016-04-14 03:25:55 +0100

    Private

--- superH/battle.php
+++ superH/battle.php
@@ -12,7 +12,24 @@
 </header>
 
 <main>
-
+<form action="insertbattle.php" method="post">
+    <p>Select the Superhero that fought in the battle</p>
+    <select name="superhero">
+        <?php
+    include($db_connect.php);
+        $sql_query = 'SELECT * FROM superheros';
+        $result = $db->query ($sql_query);
+        while ($rwo = $result->fetch_array()) {
+            $firstname = $row['firstname'];
+            $lastname = $row['lastname'];
+            $mainSuperPower = $row ['mainSuperPower'];
+            echo "<option value={$superHero}>{$firstname}{$lastname}</option>";
+            }
+?>
+    </select><br>
+    <input type="text" name="villan" placeholder="Villan Fought"><br>
+    <input ype="submit" text="Record Battle">
+</form>
 </main>
 </body>
 </html>
