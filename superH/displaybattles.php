07264e8a9d060c0c2f4cc1d40f29d7790ef126c7
uchejudeegbue <lordadmiralty@yahoo.com>
2016-04-14 03:54:38 +0100

    Private

--- superH/displaybattles.php
+++ superH/displaybattles.php
@@ -1,7 +1,39 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: UCHE EGBUE BENDAC
- * Date: 4/14/2016
- * Time: 3:35 AM
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Superhero System</title>
+</head>
+
+<body>
+<header>
+    <h1> The Super-Superhero System</h1>
+    <h2>Display all Battles</h2>
+    <p><a href="index.php"> Return to home</a></p>
+</header>
+
+<main>
+    <?php
+    include("$db_connect.php");
+
+    if (isset($_GET['id'])){
+        $superHero = $_GET['id'];
+        $sql_query = "SELECT * FROM superherobattles WHERE superHero = $superHero";
+    }
+    else{
+    $sql_query = "SELECT * FROM superherobattles";
+    }
+
+    $result = $db->query ($sql_query);
+    while ($row = $result-> fetch_array()){
+        $firstname = $row ['firstname'];
+        $lastname = $row ['lastname'];
+        $mainSuperPower = $row ['mainSuperPower'];
+        $villanFought = $row ['villanFought'];
+        echo "<article> <p>The Superhero known as <strong>{$firstname}</strong> recently fought <strong>{$villanFought}</strong>
+using <strong>{mainSuperPower}</strong></p>";
+    }
+    ?>
+</main>
+</body>
+</html>
