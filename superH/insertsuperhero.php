ac738c855f654467a8d8f5b0ca99c28a73ab7b3f
uchejudeegbue <lordadmiralty@yahoo.com>
2016-04-14 02:37:47 +0100

    Private

---
+++ superH/insertsuperhero.php
@@ -0,0 +1,15 @@
+<?php
+include("db_connect.php");
+
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$superpower = $_POST["superpower"];
+
+$sql = "INSERT INTO superheros(firstname, lastname, mainSuperPower) VALUES ('$firstname', '$lastname', '$superpower' )";
+ if (mysqli_query($db, $sql)){
+
+ }else {
+     echo "Error: " . $sql . "<br>" . mysqli_error($db);
+ }
+header ("location: index.php");
+?>
