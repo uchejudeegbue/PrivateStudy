b10d6135385c44a0c110e8699c2a734dbe29b05f
uchejudeegbue <lordadmiralty@yahoo.com>
2016-04-17 23:33:19 +0100

    Private

--- SimpleForm/connect.php
+++ SimpleForm/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 define (DB_SERVER, 'ap-cdbr-azure-east-c.cloudapp.net');
-define (DB_USERNAME, 'bbdc7d2af7a00e');
-define (DB_PASSWORD, '9a474846');
-define (DB_DATABASE, 'uchenna');
+define (DB_USERNAME, 'b80e99e6f82373');
+define (DB_PASSWORD, 'c005973d');
+define (DB_DATABASE, 'names');
 
 $db = $mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
