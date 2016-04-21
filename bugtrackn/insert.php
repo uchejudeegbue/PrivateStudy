65a34f1eabbdcbf665f9e444741788d75e77dbc8
1418349 <a@a.com>
2016-04-21 12:57:34 +0100

    Testing page

--- bugtrackn/insert.php
+++ bugtrackn/insert.php
@@ -15,12 +15,12 @@ if (empty($firstname)||(empty($lastname))){
         $sql = "INSERT INTO users (firstname, lastname) VALUES ('$firstname', '$lastname')";
 
         if (mysqli_query($db,$sql)){
-
+            header ("location: display.php");
         }
         else {
             echo "Error: " . $sql . "<br>" . mysqli_error ($db);
         }
-header ("location: display.php");
+
 
 
 ?>
