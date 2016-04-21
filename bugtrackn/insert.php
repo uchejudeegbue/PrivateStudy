bd58f65b6d845cb7ec2750596f088447bad65fb2
1418349 <a@a.com>
2016-04-21 12:54:04 +0100

    Testing page

--- bugtrackn/insert.php
+++ bugtrackn/insert.php
@@ -2,29 +2,28 @@
 
 include("connect.php");
 
-
-
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-
-
 if (empty($firstname)||(empty($lastname))){
     echo "Both fields are required";
+
 }
-    else{
+    else {
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+    }
 
 
         $sql = "INSERT INTO testing (firstname, lastname) VALUES ('$firstname', '$lastname')";
 
         if (mysqli_query($db,$sql)){
-            header ("location: display.php");
+
         }
         else {
             echo "Error: " . $sql . "<br>" . mysqli_error ($db);
         }
+header ("location: display.php");
 
 
-    }
+
 
 
 ?>
