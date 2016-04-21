c94d475126c15cd04a656c7798b8f57252e22721
1418349 <a@a.com>
2016-04-21 13:02:25 +0100

    Testing page

--- bugtrackn/connect.php
+++ bugtrackn/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 define (DB_SERVER, 'ap-cdbr-azure-east-c.cloudapp.net');
-define (DB_USERNAME, 'b80e99e6f82373');
-define (DB_PASSWORD, 'c005973d');
-define (DB_DATABASE, 'acsm_892771a76e00455');
+define (DB_USERNAME, 'b70bf26a0e51fe');
+define (DB_PASSWORD, '408b52f1');
+define (DB_DATABASE, 'acsm_581c9d4a8ca6f21');
 
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
