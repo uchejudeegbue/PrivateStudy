8dbdd1657e1f83b9b9ac6628fbca20ec4b3e30fd
1418349 <a@a.com>
2016-04-25 10:08:45 +0100

    bugTracker

---
+++ BugTracker/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bug Tracker</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <img id="logo" src="logo.png" alt="logo"/>
+
+
+    <h1>Bug Tracker</h1>
+
+    <p id = p1>Keeping track of all the pesky little bugs</p>
+
+</header>
+<content>
+    <div class="nav">
+        <nav>
+            <ul>
+                <li><a href="bugs.html">All Bug Items</a>
+                    <ul>
+                        <li><a href="addbugs.php">Add Bug</a></li>
+                        <li><a href="showbugs.php">Show Bug</a></li>
+                    </ul>
+                </li>
+                <li><a href="bugs.html">Andriod Bugs</a></li>
+                <li><a href="bugs.html">iOS Bugs</a></li>
+                <li><a href="bugs.html">Windows Bugs</a></li>
+                <li><a href="bugs.html">Insert Bugs</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="para">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            et
+            dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugait
+            nulla pariatur. Excepteur sint occaecat cupidadat non proident, sunt in culpa qui officia desertunt
+            mollit
+            anium id est laborum</p>
+    </div>
+
+</content>
+<footer>
+    Designed by Uche Jude Egbue, 2016
+</footer>
+
+</body>
+</html>
