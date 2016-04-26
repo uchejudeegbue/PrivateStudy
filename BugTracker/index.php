e596587b1e43b5cdf81d2cd471441168a4bb3bfd
1418349 <a@a.com>
2016-04-26 20:15:44 +0100

    bugs

--- BugTracker/index.php
+++ BugTracker/index.php
@@ -21,7 +21,7 @@
     <div class="nav">
         <nav>
             <ul>
-                <li><a href="showbugs.php">All Bug Items</a>
+                <li><a href="addbugs.php">All Bug Items</a>
                     <ul>
                         <li><a href="addbugs.php">Add Bug</a></li>
                         <li><a href="showbugs.php">Show Bug</a></li>
